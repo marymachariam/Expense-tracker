84bb7ccff3e3b8883c7948c1107c9a0ff99acd83
Mary <maryymachariam@gmail.com>
2026-07-25 09:05:19 +0300

    Configured for deployment

--- budget_tracker_frontend/app/lib/api.js
+++ budget_tracker_frontend/app/lib/api.js
@@ -1,6 +1,6 @@
 import { getUser } from "./auth";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 // Auth
 export async function registerUser(data) {
